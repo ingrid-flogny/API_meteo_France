77ed8db79fd994fbc716df9f05132a3ebc6e14c7
iflogny <iflogny@synergy.fr>
2024-09-05 18:05:01 +0200

    Récupération du numero de commande par API OK

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import json
 import requests
-import time
-
 from config import API_KEY
 
 base_url = "https://public-api.meteofrance.fr"
-
-# PARAMETRAGE
 num_station = 59343001
-date_start = "2024-01-08T00%3A00%3A00Z"
-date_end = "2024-08-11T00%3A00%3A00Z"
+date_start = "2024-08-01T00%3A00%3A00Z"
+date_end = "2024-09-03T00%3A00%3A00Z"
 
-def get_data(url: str, headers: json):
+
+def get_data(url: str, headers):
     response = requests.get(url, headers=headers)
+    print("Response Status Code:", response.status_code)
     if response.status_code == 200:
         return response.json()
     else:
+        print("Response Content:", response.content)
         response.raise_for_status()
 
-
 if __name__ == '__main__':
-
     url = f"{base_url}/public/DPClim/v1/commande-station/quotidienne?id-station={num_station}&date-deb-periode={date_start}&date-fin-periode={date_end}"
-    print("url", url)
+
+    if not API_KEY:
+        raise ValueError("API_KEY is missing or empty in config.py")
 
     headers = {
-        "apikey": API_KEY,
-        "accept": "*/*"
+        "accept": "*/*",
+        "Authorization": f"Bearer {API_KEY}"
     }
 
     try:
@@ -36,5 +35,3 @@ if __name__ == '__main__':
         print(f"HTTP error occurred: {http_err}")
     except Exception as err:
         print(f"An error occurred: {err}")
-
-
